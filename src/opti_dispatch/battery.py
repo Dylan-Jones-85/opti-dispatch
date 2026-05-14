9574060e6bed949c3edf3e9d9016271824540b49
Dylan Jones <dcj.jones@yahoo.com>
2026-05-14 23:54:58 +0100

    refactor: changed Battery to BatterySpec, SoC will be solver parameter

--- src/opti_dispatch/battery.py
+++ src/opti_dispatch/battery.py
@@ -1,7 +1,6 @@
-class Battery:
+class BatterySpec:
     def __init__(self, max_charge_rate: float, max_discharge_rate: float, capacity: float):
         self.max_charge_rate = max_charge_rate # MW
         self.max_discharge_rate = max_discharge_rate # MW
         self.capacity = capacity # MWh
-        self.energy_stored = 0 # MWh
 
