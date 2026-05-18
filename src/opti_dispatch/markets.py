3e051721b29e81b8241a084287db2392f9d2d641
Dylan Jones <dcj.jones@yahoo.com>
2026-05-18 19:53:11 +0100

    refactor: change market freq to interval. Make align_market_freqs return new time series frequency.

--- src/opti_dispatch/markets.py
+++ src/opti_dispatch/markets.py
@@ -7,7 +7,7 @@ import math
 class Market:
     name: str
     prices: pd.Series # datetime index
-    market_freq: pd.Timedelta # Unaffected by resampling 
+    interval: pd.Timedelta # Unaffected by resampling 
 
     def __post_init__(self):
         if not isinstance(self.prices.index, pd.DatetimeIndex):
@@ -21,14 +21,14 @@ class Market:
 
     def upsample(self, freq: pd.Timedelta) -> "Market":
         resampled_prices = self.prices.resample(freq).ffill()
-        return Market(name=self.name, prices=resampled_prices, market_freq=self.market_freq)
+        return Market(name=self.name, prices=resampled_prices, interval=self.interval)
     
-def align_market_freqs(markets: list[Market]):
-    freqs = [int(pd.Timedelta(m.market_freq).total_seconds()) for m in markets]
+def align_market_freqs(markets: list[Market])->tuple[list[Market], pd.Timedelta]:
+    freqs = [int(pd.Timedelta(m.interval).total_seconds()) for m in markets]
     # Highest common frequency = greatest common divisor
     hcf = pd.Timedelta(seconds=math.gcd(*freqs))
     resampled_markets = []
     for m in markets:
         m_r = m.upsample(hcf)
         resampled_markets.append(m_r)
-    return resampled_markets
+    return resampled_markets, hcf
