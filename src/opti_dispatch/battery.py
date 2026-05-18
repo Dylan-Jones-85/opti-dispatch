fc92d6bbf181bdaa1042c1b3b6910624e1ec6607
Dylan Jones <dcj.jones@yahoo.com>
2026-05-18 20:47:12 +0100

    feat: add from_csv constructor method to BatterySpec

--- src/opti_dispatch/battery.py
+++ src/opti_dispatch/battery.py
@@ -1,6 +1,19 @@
-class BatterySpec:
-    def __init__(self, max_charge_rate: float, max_discharge_rate: float, capacity: float):
-        self.max_charge_rate = max_charge_rate # MW
-        self.max_discharge_rate = max_discharge_rate # MW
-        self.capacity = capacity # MWh
+from dataclasses import dataclass
+import pandas as pd
 
+@dataclass
+class BatterySpec:
+    max_charge_rate: float # MW
+    max_discharge_rate: float # MW
+    capacity: float # MWh
+
+    @classmethod
+    def from_csv(cls, file_path) -> "BatterySpec":
+        df = pd.read_csv(file_path, index_col="Name")
+        
+        def get(name: str) -> float:
+            return pd.to_numeric(df.loc[name, "Value"])
+        
+        return cls(max_charge_rate=get("Max charging rate"),
+                   max_discharge_rate=get("Max discharging rate"),
+                   capacity=get("Max storage volume"))
